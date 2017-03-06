5abfa5a37948fc68bdefa4df61ef884fe1e1ec27
Antonello Provenzano <antonello@deveel.com>
2017-03-06 23:52:55 +0100

    Tests

--- test/Deveel.Rest.Client.Tests/Client/FormatTests.cs
+++ test/Deveel.Rest.Client.Tests/Client/FormatTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 using NUnit.Framework;
 
@@ -9,6 +10,19 @@ namespace Deveel.Web.Client {
 		public void FormatDeleteRequestAsHttp() {
 			var client = RestClient.Build("http://api.example.com");
 			var request = RestRequest.Build(builder => builder.Delete().To("user").WithQueryString("id", 22));
+
+			var httpString = request.ToHttpString(client);
+
+			Assert.IsNotNull(httpString);
+		}
+
+		[Test]
+		public void FormatWithFileAndBody() {
+			var client = RestClient.Build("http://api.example.com");
+			var request = RestRequest.Build(builder => builder.Post().To("user")
+				.WithBody(new RequestBody("data", new { a = 1, b = "two" }, ContentFormat.KeyValue))
+				.WithFile(new RequestFile("file", "file.png", "image/png", new MemoryStream()))
+				.Returns<dynamic>());
 
 			var httpString = request.ToHttpString(client);
 
